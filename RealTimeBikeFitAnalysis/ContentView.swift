2ecde418023f705e8096a190255b2b82256886a2
Mkrefting <maxkrefting@yahoo.co.uk>
2021-03-02 22:01:32 +0000

    Initial Commit

---
+++ RealTimeBikeFitAnalysis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealTimeBikeFitAnalysis
+//
+//  Created by Max Krefting on 02/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealTimeBikeFitAnalysis/RealTimeBikeFitAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeBikeFitAnalysisApp.swift
+//  RealTimeBikeFitAnalysis
+//
+//  Created by Max Krefting on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeBikeFitAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
