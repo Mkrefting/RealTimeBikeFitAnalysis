8f0604a2680e898c8acf0534d35e258980cd78e9
Mkrefting <maxkrefting@yahoo.co.uk>
2021-03-05 21:49:15 +0000

    Cleaning up the CameraViewController

--- RealTimeBikeFitAnalysis/CameraViewController.swift
+++ RealTimeBikeFitAnalysis/CameraViewController.swift
@@ -9,33 +9,100 @@ import SwiftUI
 import UIKit
 import AVFoundation
 
-final class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+final class CameraViewController: UIViewController {
+
+    // MARK: View Controller Life Cycle
     
-    private var captureSession: AVCaptureSession! = AVCaptureSession()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // check video authorization status - video access is required
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+            case .authorized:
+                // user has previously granted access to camera
+                break
+            case .notDetermined:
+                // user has not yet been presented with the option to grant video access. Suspend the session queue to delay session setup until the access request has completed
+                sessionQueue.suspend()
+                AVCaptureDevice.requestAccess(for: .video, completionHandler: {granted in
+                    if granted{
+                        self.sessionQueue.resume()
+                    }
+                })
+            default:
+                // user has previously denied access (or cannot use camera due to restrictions)
+                return
+        }
+        
+        // Setup the capture session
+        // We do not perform these tasks on the main queue because AVCaptureSession.startRunning() is a blocking call, which can take a long time. Dispatch session setup to the sessionQueue, so the main queue is not blocked, which keeps the UI responsive.
+        sessionQueue.async{
+            self.configureSession()
+        }
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sessionQueue.async{
+            self.session.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        sessionQueue.async{
+            self.session.stopRunning()
+        }
+        super.viewWillDisappear(animated)
+    }
+    
+    override var shouldAutorotate: Bool { false }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {return .landscapeRight}
+
+    
+    // MARK: Session Management
+    
+    private var session = AVCaptureSession()
+    
+    // Communicate with the session and other session objects on this queue
+    private let sessionQueue = DispatchQueue(label: "session queue")
+
     private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
-        let preview = AVCaptureVideoPreviewLayer(session:self.captureSession)
+        let preview = AVCaptureVideoPreviewLayer(session:self.session)
         preview.videoGravity = .resizeAspect    // experiment with .resizeAspectFill
         preview.connection?.videoOrientation = .landscapeRight
         return preview
     }()
     private let videoOutput = AVCaptureVideoDataOutput()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.addCameraInput()
+    
+    // MARK: ConfigureSession
+    // To be called on the session queue
+    private func configureSession(){
+        session.beginConfiguration()
+        self.addVideoInput()
         self.addPreviewLayer()
         self.addVideoOutput()
-        self.captureSession.startRunning()
+        session.commitConfiguration()
     }
     
-    func addCameraInput(){
-        guard let videoCaptureDevice = AVCaptureDevice.default(for:.video) else {return}
+    func addVideoInput(){
+        // 0. Set session preset (video quality) - lower is better for ml model efficiency
+        // 1. Find input
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes:[.builtInTrueDepthCamera,.builtInDualCamera,.builtInWideAngleCamera], mediaType:.video, position:.back)
+        guard let videoDevice = discoverySession.devices.first else {
+            print("Error: Video device is unavailable")
+            return
+        }
+        // 2. Get input
         let videoInput: AVCaptureDeviceInput
-        do {
-            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {return}
-        if (self.captureSession.canAddInput(videoInput)) {
-            self.captureSession.addInput(videoInput)
+        do {videoInput = try AVCaptureDeviceInput(device: videoDevice)
+        } catch {
+            print("Error: Video device input is unavailable")
+            return
+        }
+        // 3. Add input
+        if (self.session.canAddInput(videoInput)){
+            self.session.addInput(videoInput)
         } else {return}
     }
 
@@ -47,24 +114,13 @@ final class CameraViewController: UIViewController, AVCaptureVideoDataOutputSamp
     func addVideoOutput(){
         self.videoOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
         self.videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "my.image.handling.queue"))
-        self.captureSession.addOutput(self.videoOutput)
+        self.session.addOutput(self.videoOutput)
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+}
 
-        if (self.captureSession?.isRunning == false) {
-            self.captureSession.startRunning()
-        }
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        if (self.captureSession?.isRunning == true) {
-            self.captureSession.stopRunning()
-        }
-    }
-
+// MARK: Process Image Frames
+extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput,
                        didOutput sampleBuffer: CMSampleBuffer,
                        from connection: AVCaptureConnection) {
@@ -74,9 +130,6 @@ final class CameraViewController: UIViewController, AVCaptureVideoDataOutputSamp
         }
         // process image here
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {return .landscapeRight}
-    
 }
 
 extension CameraViewController: UIViewControllerRepresentable {
